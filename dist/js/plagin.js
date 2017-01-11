b7e6ca359f4bda30127c120dd3806be785c5a948
Sabbash <Sabbash23@Gmail.com>
2017-01-11 02:48:58 +0200

    update

--- dist/js/plagin.js
+++ dist/js/plagin.js
@@ -2,26 +2,27 @@
 (function ($) {
     $(window).on('load', function () {
         var total_slide = $('.slide').length;
-        if (total_slide < 10) {
+        if (total_slide < 10 && total_slide != 1) {
             $('[data-slide="total-slides"]').text('/' + '0' + total_slide);
-        } else {
+        } else if (total_slide != 1) {
             $('[data-slide="total-slides"]').text('/' + total_slide);
         }
-
     });
 })(jQuery);
 
 //current slide
 (function ($) {
     $(window).on('load', function () {
-        var slide = 1;
+        total = $('.slide').length;
+        if (total != 1) {
+            $('[data-slide="current-slide"]').text('0' + (total - (total -1)));
+        }
         $('.slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
-            slide = nextSlide + 1;
-            console.log(slide);
-            if (slide < 10) {
-                $('[data-slide="current-slide"]').text('0' + slide);
+            $slide = nextSlide + 1;
+            if ($slide < 10) {
+                $('[data-slide="current-slide"]').text('0' + $slide);
             } else {
-                $('[data-slide="current-slide"]').text(slide);
+                $('[data-slide="current-slide"]').text($slide);
             }
         });
     });
@@ -36,7 +37,7 @@
     });
 })(jQuery);
 
-//
+//description button
 (function ($) {
     $(window).on('load', function () {
         $('[data-btn="description"]').on('click', function () {
